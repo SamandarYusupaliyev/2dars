6e3665790cae484cf8b1dde730901f08e622dfe6
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-03-27 21:07:34 +0500

    papka

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [tasks,setTasks]= useState([
+  {
+    id: 1,
+    title: "Reading books",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, minus.",
+  },
+  {
+    id: 2,
+    title: "Cleaning rooms",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, minus.",
+  },
+  {
+    id: 3,
+    title: "Playing CS2",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, minus.",
+  },
+
+  ]);
+  const deleteTask =(id)=>{
+    const filteredTask=tasks.filter((task)=>{
+      return task.id !== id;
+    });
+     setTasks(filteredTask);
+  };
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+      {tasks.map((task)=>{
+        return(
+          <li key={task.id}>
+            <h3 className='title'>{task.title}</h3>
+            <p className='text'>{task.body}</p>
+            <button className='btn' onClick={()=>deleteTask(task.id)}>Delete</button>
+         </li>
+        ); 
+      })}
+      </ul>
     </div>
   );
-}
 
+};
 export default App;
